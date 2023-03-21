5f58bfb996ac9a6515826fa724724b9efe41f8b5
Smiles <chenmay1696@gmail.com>
2023-03-21 19:10:01 +0200

    add folder

---
+++ src/main/java/demo/HelloController.java
@@ -0,0 +1,17 @@
+package demo;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	@RequestMapping(
+		path = {"/hello"},
+		method = {RequestMethod.GET},
+		produces = {MediaType.APPLICATION_JSON_VALUE})
+	public Message hello () {
+		return new Message("Hello World!");
+	}
+}
