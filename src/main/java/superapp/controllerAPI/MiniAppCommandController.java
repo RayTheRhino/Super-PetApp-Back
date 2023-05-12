d5422cb08230b29f81108d5e1b3f5dc4f9b5585e
Neidav316 <neidav86@gmail.com>
2023-05-12 18:25:47 +0300

    added and updated the controllers for url flags - add search by mapping for type, alias and location for objects  - need to create new services function to apply search  - need to update existing functions to use the flags for pagination

--- src/main/java/superapp/controllerAPI/MiniAppCommandController.java
+++ src/main/java/superapp/controllerAPI/MiniAppCommandController.java
@@ -1,14 +1,10 @@
 package superapp.controllerAPI;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 import superapp.bounderies.CommandId;
 import superapp.bounderies.MiniAppCommandBoundary;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 import superapp.logic.MiniappCommandsService;
 
 
@@ -28,7 +24,8 @@ public class MiniAppCommandController {
             produces = {MediaType.APPLICATION_JSON_VALUE})
     public Object invokeMiniApp (
             @PathVariable("miniAppName") String miniAppName,
-            @RequestBody MiniAppCommandBoundary miniAppCommandBoundary) {
+            @RequestBody MiniAppCommandBoundary miniAppCommandBoundary,
+            @RequestParam (name = "async", required = false, defaultValue = "false") boolean async)) {
 
         miniAppCommandBoundary.setCommandId(new CommandId("SuperPetApp", miniAppName, ""));
         return miniappCommandsService.invokeCommand(miniAppCommandBoundary);
--- src/main/java/superapp/controllerAPI/SuperAppObjectsController.java
+++ src/main/java/superapp/controllerAPI/SuperAppObjectsController.java
@@ -11,51 +11,104 @@ import java.util.List;
 @RestController
 public class SuperAppObjectsController {
 
-        private ObjectsService objectsService;
+    private ObjectsService objectsService;
     @Autowired
     public void setObjectsService(ObjectsService objectsService){
         this.objectsService = objectsService;
     }
-        @RequestMapping(
-                path = {"/superapp/objects"},
-                method = {RequestMethod.POST},
-                produces = {MediaType.APPLICATION_JSON_VALUE},
-                consumes = {MediaType.APPLICATION_JSON_VALUE}
-        )
+    @RequestMapping(
+            path = {"/superapp/objects"},
+            method = {RequestMethod.POST},
+            produces = {MediaType.APPLICATION_JSON_VALUE},
+            consumes = {MediaType.APPLICATION_JSON_VALUE}
+    )
 
-        public ObjectBoundary createObject(@RequestBody ObjectBoundary input){
-                return objectsService.CreateObject(input);
-        }
+    public ObjectBoundary createObject(
+            @RequestBody ObjectBoundary input){
+            return objectsService.CreateObject(input);
+    }
 
-        @RequestMapping(
-                path = {"/superapp/objects/{superapp}/{InternalObjectId}"},
-                method = {RequestMethod.PUT},
-                consumes = {MediaType.APPLICATION_JSON_VALUE}
-        )
-        public void updateObject ( @PathVariable("superapp") String superapp,
-                                   @PathVariable("InternalObjectId") String InternalObjectId,
-                                   @RequestBody ObjectBoundary input) {
-                objectsService.updateObject(superapp,InternalObjectId,input);
-        }
+    @RequestMapping(
+            path = {"/superapp/objects/{superapp}/{InternalObjectId}"},
+            method = {RequestMethod.PUT},
+            consumes = {MediaType.APPLICATION_JSON_VALUE}
+    )
+    public void updateObject (
+            @PathVariable("superapp") String superapp,
+            @PathVariable("InternalObjectId") String InternalObjectId,
+            @RequestBody ObjectBoundary input,
+            @RequestParam (name = "userSuperapp") String userSuperapp,
+            @RequestParam (name = "userEmail") String email) {
 
-        @RequestMapping(
-                path = {"/superapp/objects/{superapp}/{InternalObjectId}"},
-                method = {RequestMethod.GET},
-                produces = {MediaType.APPLICATION_JSON_VALUE})
-        public ObjectBoundary retrieveObject(
-                @PathVariable("superapp") String superapp,
-                @PathVariable("InternalObjectId") String InternalObjectId) {
-                return objectsService.getSpecificObject(superapp,InternalObjectId);
-        }
+        objectsService.updateObject(superapp,InternalObjectId,input);//TODO: Update Functions
+    }
 
-        @RequestMapping(
-                path = {"/superapp/objects"},
-                method = {RequestMethod.GET},
-                produces = {MediaType.APPLICATION_JSON_VALUE})
-        public ObjectBoundary[] getAllObjects (){
-            List<ObjectBoundary> allObjects = objectsService.getAllObjects();
-            return allObjects.toArray(new ObjectBoundary[0]);
+    @RequestMapping(
+            path = {"/superapp/objects/{superapp}/{InternalObjectId}"},
+            method = {RequestMethod.GET},
+            produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ObjectBoundary retrieveObject(
+            @PathVariable("superapp") String superapp,
+            @PathVariable("InternalObjectId") String InternalObjectId,
+            @RequestParam (name = "userSuperapp") String userSuperapp,
+            @RequestParam (name = "userEmail") String email) {
+            return objectsService.getSpecificObject(superapp,InternalObjectId); //TODO: Update Functions
+    }
 
-        }
+    @RequestMapping(
+            path = {"/superapp/objects/search/byType/{type}"},
+            method = {RequestMethod.GET},
+            produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ObjectBoundary[] retrieveObjectByType(
+            @PathVariable("type") String type,
+            @RequestParam (name = "userSuperapp") String superapp,
+            @RequestParam (name = "userEmail") String email,
+            @RequestParam (name = "size", required = false, defaultValue = "10") int size,
+            @RequestParam (name = "page", required = false, defaultValue = "0") int page) {
+        return objectsService.getSpecificObjectsByType(superapp,InternalObjectId); // TODO: create the new get functions
+    }
+
+    @RequestMapping(
+            path = {"/superapp/objects/search/byAlias/{alias}"},
+            method = {RequestMethod.GET},
+            produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ObjectBoundary[] retrieveObjectByAlias(
+            @PathVariable("alias") String alias,
+            @RequestParam (name = "userSuperapp") String superapp,
+            @RequestParam (name = "userEmail") String email,
+            @RequestParam (name = "size", required = false, defaultValue = "10") int size,
+            @RequestParam (name = "page", required = false, defaultValue = "0") int page){
+    return objectsService.getSpecificObjectsByAlias(superapp,email); // TODO: create the new get functions
+    }
+    @RequestMapping(
+            path = {"/superapp/objects/search/byLocation/{lat}/{lng}/{distance}"},
+            method = {RequestMethod.GET},
+            produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ObjectBoundary[] retrieveObject(
+            @PathVariable("lat") double lat,
+            @PathVariable("lng") double lng,
+            @PathVariable("distance") double distance,
+            @RequestParam (name = "units", required = false, defaultValue = "NEUTRAL") String distanceUnits,
+            @RequestParam (name = "userSuperapp") String superapp,
+            @RequestParam (name = "userEmail") String email,
+            @RequestParam (name = "size", required = false, defaultValue = "10") int size,
+            @RequestParam (name = "page", required = false, defaultValue = "0") int page){
+    return objectsService.getSpecificObjectsByLocation(superapp,InternalObjectId);// TODO: create the new get functions
+    }
+
+    @RequestMapping(
+            path = {"/superapp/objects"},
+            method = {RequestMethod.GET},
+            produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ObjectBoundary[] getAllObjects (
+            @RequestParam (name = "userSuperapp") String userSuperapp,
+            @RequestParam (name = "userEmail") String email,
+            @RequestParam (name = "size", required = false, defaultValue = "10") int size,
+            @RequestParam (name = "page", required = false, defaultValue = "0") int page
+    ){
+        List<ObjectBoundary> allObjects = objectsService.getAllObjects(); //TODO: Update Functions
+        return allObjects.toArray(new ObjectBoundary[0]);
 
     }
+
+}
